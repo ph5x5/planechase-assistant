9d1e59aa3c8925dc65cdc9074af24c7cab55deb9
Andrey Murzich <andrey.murzich@oxagile.com>
2022-07-18 02:39:28 +0300

    Testing basic functionality

--- assistant/views.py
+++ assistant/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from scryfall import api
 
 
 def index(request):
-    # return HttpResponse('Hello from Python!')
+    number = request.GET['number']
+    image_url = api.get_card_image_url(number)
     return render(request, "index.html")
---
+++ scryfall/api.py
@@ -0,0 +1,27 @@
+import requests
+from decouple import config
+
+
+def obtain_card_list():
+    error = ''
+    scryfall_url = config('SCRYFALL_URL')
+    url = f"{scryfall_url}/cards/search?q=set%3Aopca"
+    try:
+        response = requests.get(url)
+        card_list = response.json()['data']
+    except e:
+        error = f"Can't access the Scryfall API: {e}"
+        card_list = []
+    return card_list, error
+
+
+def get_card_image_url(number):
+    error = ''
+    card_list, error = get_card_list()
+    if not error:
+        try:
+            image_url = card_list[number - 1]['image_uris']['normal']
+        except e:
+            error = f"Can't get the information from the Scryfall response: {e}"
+            image_url = ''
+    return image_url, error
