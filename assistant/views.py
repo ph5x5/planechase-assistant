10390f8f4d016dfa6364941de9acf8a2fc6e1351
Andrey Murzich <andrey.murzich@oxagile.com>
2022-07-18 02:59:13 +0300

    Adding error verification

--- assistant/views.py
+++ assistant/views.py
@@ -6,6 +6,7 @@ from scryfall import api
 def index(request):
     args = {}
     number = request.GET['number']
-    image_url = api.get_card_image_url(number)
+    image_url, error = api.get_card_image_url(number)
     args['image_url'] = image_url
+    args['error'] = error
     return render(request, "index.html", args)
