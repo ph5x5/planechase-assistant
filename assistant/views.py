4fdea3f00dbf829f1ab6824b4d5f8ba0d0272bad
Andrey Murzich <andrey.murzich@oxagile.com>
2022-07-18 11:17:56 +0300

    Adding random number generator

--- assistant/views.py
+++ assistant/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from scryfall import api
+import random
 
 
 def index(request):
     args = {}
     number = request.GET['number']
-    image_url, error = api.get_card_image_url(number)
+    random = request.GET['random']
+
+    random_bool = bool(random)
+    if random:
+        number = random.randint(1, 86)
+
+    if number:
+        image_url, error = api.get_card_image_url(number)
+    else:
+        image_url, error = '', ''
     args['image_url'] = image_url
     args['error'] = error
     return render(request, "index.html", args)
